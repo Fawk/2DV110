f408c81a22d9123c1fa17e92aa097c8353d75fb5
ok222ax <ok222ax@student.lnu.se>
2014-11-25 18:35:01 +0100

    GREEN
    Assignment 1

--- src/Assignment1/Memory.java
+++ src/Assignment1/Memory.java
@@ -2,7 +2,19 @@ package Assignment1;
 
 public class Memory {
 
+	public Memory(int pairs) {
+		if(pairs <= 0 || pairs > 10) {
+			throw new IllegalArgumentException();
+		}
+	}
 
+	public int getPairs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-
+	public int getSlots() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
