ecca047f1e80ed8d82a5fbc313cf27b5fca99545
ok222ax <ok222ax@student.lnu.se>
2014-11-25 19:06:06 +0100

    RED
    Assignment 1

--- tests/Assignment1/MemoryTests.java
+++ tests/Assignment1/MemoryTests.java
@@ -12,14 +12,33 @@ public class MemoryTests {
 		new Memory(0);
 	}
 	
+	@Test(expected=IllegalArgumentException.class)
+	public void testTooLargeMemorySize() throws IllegalArgumentException {
+		new Memory(11);
+	}
+	
 	@Test
 	public void testMemorySize() {
-		Memory mockedMemory = mock(Memory.class);
-		when(mockedMemory.getPairs()).thenReturn(5);
-		when(mockedMemory.getSlots()).thenReturn(10);
+		Memory m = new Memory(5);
 		
-		assert(mockedMemory.getPairs() == 5);
-		assert(mockedMemory.getSlots() == 10);
+		assert(m.getPairs() == 5);
+		assert(m.getSlots() == 10);
+	}
+	
+	@Test
+	public void testMemorySlot() {
+		Memory m = new Memory(5);
+		
+		char[] c = { 'A', 'B', 'C', 'D', 'E' };
+		
+		m.SetFakeOrder(c);
+		
+		for(int i = 0; i < 5; i++) {
+			MemorySlot mockedSlot = mock(MemorySlot.class);
+			when(mockedSlot.getValue()).thenReturn(c[i]);
+			MemorySlot realSlot = m.getSlot(0);
+			assert(m.sameSlotValue(mockedSlot, realSlot));
+		}
 	}
 	
 }
